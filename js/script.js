21d1bd86515c65ea77cc441712b36700b64c29dc
Th30s <yahir.garcia@uasdc.org>
2019-11-18 23:08:28 +0000

    Added computer choice and userchoice functionality

--- js/script.js
+++ js/script.js
@@ -4,9 +4,24 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-let rpc = ["rock", "paper", "scissor"]
+
+console.log();
 $("#shoot").click(function(){
-    $("#userChoice").text($("#input").val());
+    //Computer choice//
+    let computerCoice = Math.ceil(Math.random() * 3);
+    if (computerCoice === 1) {
+    $("#computerChoice").text("rock");
+    }else if (computerCoice === 2){
+    $("#computerChoice").text("paper");
+    }else if (computerCoice === 3){
+    $("#computerChoice").text("scissor");
+    };
+    console.log(computerCoice);
+    //User Choice//
+    let userChoice = $("#input").val();
+    $("#userChoice").text(userChoice.toLowerCase());
+    //Winner
+    if (computerCoice === )
 });
 
 // DOCUMENT READY FUNCTION BELOW
